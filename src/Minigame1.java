6f984d46a548abfd4318dc730ab0afb3c9e759d5
Jonathan <fulcherandjonathan@gmail.com>
2020-10-16 19:33:06 +0200

    Minigame1 angefangen. Alte Klassen nach /bin verschoben.

--- src/Minigame1.java
+++ src/Minigame1.java
@@ -4,34 +4,73 @@ import ea.Knoten;
 public class Minigame1 extends Knoten {
 
     private boolean activ = false;
+
+    private int fallingObjectCount = 10;
     private int score =0;
+    private int counter =0;
 
 
-    private Bild BackgroundPic;
-    private int FallingCount = 10;
-    private Bild[] FallingObjects = new Bild[FallingCount];
+    private Bild backgroundImgObject;
+    private Bild[] FallingObjects = new Bild[fallingObjectCount];
+    private String backgroundImgPath = "./Assets/StartingScreen/Forest.jpg";
 
+    private int offsetX;//letzendlich PlayerposX
+    private int offsetY;//letzendlich PlayerposY
+
+
+
+
+
+    public void tick(){
+        if(activ){
+            int x = offsetX + (int)(Math.random()* MAIN.x);
+            //FallingObjects[counter].
+
+            counter++;
+        }
+
+    }
 
     public Minigame1(){
-        BackgroundPic = new Bild(0,0,"./Assets/StartingScreen/Forest.jpg");
-        this.add(BackgroundPic);
+        try{
+            backgroundImgObject = new Bild(0,0,backgroundImgPath);
+        }
+        catch  (Exception e){
+            System.out.println("Fehler in der Mnigame Klasse: Bild bei " + backgroundImgPath + " kann nicht gefunden werden!");
+            System.out.println(e);
+        }
+        this.add(backgroundImgObject);
 
         InitFallingbjects();
     }
 
     public void InitFallingbjects(){
-        for(int i= 0;i<FallingCount;i++){
+        for(int i= 0;i<fallingObjectCount;i++){
+
             FallingObjects[i] = new Bild(0,0,"./Assets/MouseC.png");
             FallingObjects[i].sichtbarSetzen(false);
+            FallingObjects[i].newtonschMachen();
             this.add(FallingObjects[i]);
         }
     }
+    public void centerBackground() {
+        int imgPosX = offsetX + MAIN.x - ((int)backgroundImgObject.getBreite()/2);
+        int imgPosY = offsetY + MAIN.y - ((int)backgroundImgObject.getHoehe()/2);
+        backgroundImgObject.positionSetzen(imgPosX,imgPosY);
+    }
 
-    public void startGame(){
+    public void startGame(int x, int y){
+        setOffset(x,y);
+        centerBackground();
         if(!activ){
             activ = true;
         }
     }
+    public void setOffset(int x, int y){
+        offsetX = x;
+        offsetY = y;
+    }
+
 
     public boolean isActiv() {
         return activ;

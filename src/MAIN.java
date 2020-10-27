1c0f90ef77644bd7262a7a12ad6bcab61b3affb8
Jonathan <fulcherandjonathan@gmail.com>
2020-10-27 22:11:09 +0100

    Solider Fortschritt am Minigame2(Gluecksspiel). Die Reihen sind jz gemischt und ein Jackpot kann pro Reihe ausgerechnet werden.

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Erzeugt das SPIEL Objekt und setzt Fenster Einstellungen.
  * Es ist die höchste Klasse in der Hierarchie
@@ -14,8 +19,25 @@ public class MAIN {
     public static int whiteThreshold = 250;// für die Kolliderbildbearbeitung
 
     public static void main(String[] args){
+        //System.out.println("Random Array = " + Arrays.toString(giveShuffeldArray(10)));
         SPIEL spiel  = new SPIEL();
         spiel.fokusSetzten();
+
+
+    }
+
+    private static int[] giveShuffeldArray(int size){
+        int[] arraySolution = new int[size];
+        List<Integer> solution = new ArrayList<>();
+        for (int i = 0; i < size; i++){ //to generate from 0-10 inclusive.
+            //For 0-9 inclusive, remove the = on the <=
+            solution.add(i);
+        }
+        Collections.shuffle(solution);
+        for (int i = 0; i < solution.size(); i++) {
+            arraySolution[i] = solution.get(i);
+        }
+        return arraySolution;
 
     }
 
--- src/Minigame2.java
+++ src/Minigame2.java
@@ -1,8 +1,10 @@
 import ea.Knoten;
 import ea.Bild;
 import ea.Punkt;
+import ea.Sound;
 
-import java.util.Random;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Minigame2 extends Knoten {
 
@@ -38,6 +40,10 @@ public class Minigame2 extends Knoten {
     private Bild[] row1_tileImgs = new Bild[tileCount];
     private Bild[] row2_tileImgs = new Bild[tileCount];
 
+    private int[] row0_tileImgsPointer = new int[tileCount];
+    private int[] row1_tileImgsPointer = new int[tileCount];
+    private int[] row2_tileImgsPointer = new int[tileCount];
+
     private boolean rolling = false;
 
     private int decelerationCounterOriginal = 80;
@@ -49,6 +55,7 @@ public class Minigame2 extends Knoten {
     public Minigame2(Player AP){
         this.AP = AP;
         InitImgs();
+
     }
 
 
@@ -89,13 +96,41 @@ public class Minigame2 extends Knoten {
         //für das FrameBild GANZ OBEN
         try {
             String path = mainPath  + "frame.png";
-            frameImg = new Bild(path);
+            frameImg = new Bild(offsetX,offsetY,path);
             this.add(frameImg);
             frameImg.sichtbarSetzen(false);
         }
         catch(Exception e){
             System.out.println("Minigame2: "  + e);
         }
+    }
+
+    private void shufflePointers(){
+        /*
+        for(int i = 0; i < tileCount; i++){
+            int r = (int)(Math.random() * ((tileCount-1) + 1));
+            //System.out.println("Random r: " + r);
+            while(contains(row0_tileImgsPointer,r) && i!=0){
+                //System.out.println("Gibt es schon , nochmal neu, Stelle i = " + i);
+                //System.out.println("Minigame: Bis jz: " + Arrays.toString(row0_tileImgsPointer));
+                r = (int)(Math.random() * tileCount);
+            }
+            row0_tileImgsPointer[i] = r;
+        }
+        System.out.println("Minigame: row0_tileImgsPointer final: " + Arrays.toString(row0_tileImgsPointer));
+         */
+        row0_tileImgsPointer = giveShuffeldArray(tileCount);
+        System.out.println("Minigame: Random Liste0:" + Arrays.toString(row0_tileImgsPointer));
+
+        row1_tileImgsPointer = giveShuffeldArray(tileCount);
+        System.out.println("Minigame: Random Liste1:" + Arrays.toString(row1_tileImgsPointer));
+
+        row2_tileImgsPointer = giveShuffeldArray(tileCount);
+        System.out.println("Minigame: Random Liste2:" + Arrays.toString(row2_tileImgsPointer));
+
+
+        //System.out.println("Minigame: Random Liste:" + Arrays.toString(row0_tileImgsPointer));
+        //BSP:. row0_tileImgsPointer = [2, 7, 5, 1, 6, 4, 3, 0]
     }
 
     /*
@@ -125,46 +160,57 @@ public class Minigame2 extends Knoten {
         row2_firstPos = 0;
         rolling = true;
         speed = originalSpeed;
-        setRows();
         decelerationCounter = decelerationCounterOriginal;
+
+        shufflePointers();
+        setRows();
     }
     public void tick(){
         if(active && rolling){
             switch(rowPos){
                 case(0):
+                    //System.out.println("Erstes Element = " + row0_firstPos + " und damit Bild Nummer " + row0_tileImgsPointer[row0_firstPos]);
                     for(int i = 0; i< tileCount; i++){
                         //row0_tileImgs[i].sichtbarSetzen(true);
-                        System.out.println("Minigame2: Row0 wird nach unten verschoben");
-                        row0_tileImgs[i].verschieben(0,speed); // nach oben verschieben
+                        //System.out.println("Minigame2: Row0 wird nach unten verschoben");
+                        row0_tileImgs[row0_tileImgsPointer[i]].verschieben(0,speed); // nach oben verschieben
                     }
-                    if(row0_tileImgs[row0_firstPos].getY() >= bottomEdge + offsetY){
-                        row0_tileImgs[row0_firstPos].verschieben(0,-(tileCount)*tileSize);//obenstellen
+                    if(row0_tileImgs[row0_tileImgsPointer[row0_firstPos]].getY() >= bottomEdge + offsetY){
+                        row0_tileImgs[row0_tileImgsPointer[row0_firstPos]].verschieben(0,-(tileCount)*tileSize);//obenstellen
                         row0_firstPos++;//das naechste bild als ersten angeben
                         if(row0_firstPos>= tileCount){
                             row0_firstPos = 0;
                         }
+                        //System.out.println("Neues erstes element = " + row0_firstPos + " und damit Bild Nummer " + row0_tileImgsPointer[row0_firstPos]);
                     }
                     break;
+
+
                 case(1):
                     for(int i = 0; i< tileCount; i++){
                         //row0_tileImgs[i].sichtbarSetzen(true);
-                        row1_tileImgs[i].verschieben(0,speed); // nach oben verschieben
+                        //float delta = (bottomEdge - row1_tileImgs[row1_firstPos].getY());
+                        //row1_tileImgs[i].verschieben(0,(float)(delta*0.01)); // nach oben verschieben
+                        row1_tileImgs[row1_tileImgsPointer[i]].verschieben(0,speed); // nach oben verschieben
                     }
-                    if(row1_tileImgs[row1_firstPos].getY() >= bottomEdge + offsetY){
-                        row1_tileImgs[row1_firstPos].verschieben(0,-(tileCount)*tileSize);//obenstellen
+                    if(row1_tileImgs[row1_tileImgsPointer[row1_firstPos]].getY() >= bottomEdge + offsetY){
+
+                        row1_tileImgs[row1_tileImgsPointer[row1_firstPos]].verschieben(0,-(tileCount)*tileSize);//obenstellen
                         row1_firstPos++;//das naechste bild als ersten angeben
                         if(row1_firstPos>= tileCount){
                             row1_firstPos = 0;
                         }
                     }
                     break;
+
+
                 case(2):
                     for(int i = 0; i< tileCount; i++){
                         //row0_tileImgs[i].sichtbarSetzen(true);
-                        row2_tileImgs[i].verschieben(0,speed); // nach oben verschieben
+                        row2_tileImgs[row2_tileImgsPointer[i]].verschieben(0,speed); // nach oben verschieben
                     }
-                    if(row2_tileImgs[row2_firstPos].getY() >= bottomEdge + offsetY){
-                        row2_tileImgs[row2_firstPos].verschieben(0,-(tileCount)*tileSize);//obenstellen
+                    if(row2_tileImgs[row2_tileImgsPointer[row2_firstPos]].getY() >= bottomEdge + offsetY){
+                        row2_tileImgs[row2_tileImgsPointer[row2_firstPos]].verschieben(0,-(tileCount)*tileSize);//obenstellen
                         row2_firstPos++;//das naechste bild als ersten angeben
                         if(row2_firstPos>= tileCount){
                             row2_firstPos = 0;
@@ -183,7 +229,9 @@ public class Minigame2 extends Knoten {
             if(speed <= 0){
                 if(rowPos >= 2){ //falls noch nicht reihe 2 fdann eine reihe weiter mit speed = 10;
                     System.out.println("Minigame2: " + rowPos + " ist jz unten angekommen");
+                    //Game zu ende
                     rolling = false;
+                    System.out.println("An unteresten Stelle ist der Jackpot wert = " + Arrays.toString(analyseJackpot()));
                 }
                 else{
                     System.out.println("Minigame2: " + rowPos + " ist jz unten angekommen");
@@ -196,32 +244,96 @@ public class Minigame2 extends Knoten {
         }
     }
 
+    /**
+     * Untersucht alle angezeigten(je nach der var checks) Zeilen auch Jackpots mit der Methode checkJackpots(pos);
+     * Gibt nur das Ergebnis dieser Methode als Liste zurück und kümmert sich über row0_firstPos Spezialfälle.
+     * @return Int[var checks] mit allen ergebnissen des checkJackpots Mehtodeaufrufs.
+     */
+    private int[] analyseJackpot(){
+        int check = 4;//vier Reihen werden angezeigt
+        int[] result = new int[check];
+
+        /*
+        for(int i = 0;i<check;i++){
+            int pos = row0_firstPos - i;
+            if(pos<0){
+                pos = tileCount + pos;
+            }
+            System.out.println("Minigame2: Testet Reihe mit i(" + i+ "), welche eig. Reihe " + pos + " ist.");
+            result[i] = checkJackpot(pos);
+        }
+        */
+        for(int i = 0;i<check;i++){
+            int pos = row0_firstPos + i;
+            if(pos>tileCount-1){
+                pos = pos-tileCount;
+            }
+            System.out.println("Minigame2: Testet Reihe mit i(" + i+ "), welche eig. Reihe " + pos + " ist.");
+            result[i] = checkJackpot(pos);
+        }
+
+        return result;
+    }
+    /**
+     * Gibt mir für die zeile POS den Jackpot indikator zurück
+     * @param pos Zeile die verglichen werden soll;
+     * @return gibt mit entweder 0,1,2 oder 3 zurück je nach Gewinn/Jackpot
+     */
+    private int checkJackpot(int pos){
+
+        int i = pos;
+        int jackpot = 0;
+        if(row0_tileImgsPointer[i] == row1_tileImgsPointer[i] && row0_tileImgsPointer[i] == row2_tileImgsPointer[i] && row1_tileImgsPointer[i] == row2_tileImgsPointer[i]){
+            jackpot = 3;
+        }
+        else if(row0_tileImgsPointer[i] == row1_tileImgsPointer[i]){ //links und mitte gleich
+            jackpot = 1;
+        }
+        else if(row1_tileImgsPointer[i] == row2_tileImgsPointer[i]){ //mitte und rechts gleich
+            jackpot = 2;
+        }
+
+       return jackpot;
+    }
+
     private void setRows(){
         for(int i = 0; i< tileCount; i++){
-            row0_tileImgs[i].sichtbarSetzen(true);
-            row0_tileImgs[i].positionSetzen(row0_TL_POS);
-            row0_tileImgs[i].verschieben(offsetX,offsetY);
-            row0_tileImgs[i].verschieben(0,-(i+1)*tileSize); // nach oben verschieben
+            row0_tileImgs[row0_tileImgsPointer[i]].sichtbarSetzen(true);
+            row0_tileImgs[row0_tileImgsPointer[i]].positionSetzen(row0_TL_POS);
+            row0_tileImgs[row0_tileImgsPointer[i]].verschieben(offsetX,offsetY);
+            row0_tileImgs[row0_tileImgsPointer[i]].verschieben(0,-(i+1)*tileSize); // nach oben verschieben
         }
         for(int i = 0; i< tileCount; i++){
-            row1_tileImgs[i].sichtbarSetzen(true);
-            row1_tileImgs[i].positionSetzen(row1_TL_POS);
-            row1_tileImgs[i].verschieben(offsetX,offsetY);
-            row1_tileImgs[i].verschieben(0,-(i+1)*tileSize); // nach oben verschieben
+            row1_tileImgs[row1_tileImgsPointer[i]].sichtbarSetzen(true);
+            row1_tileImgs[row1_tileImgsPointer[i]].positionSetzen(row1_TL_POS);
+            row1_tileImgs[row1_tileImgsPointer[i]].verschieben(offsetX,offsetY);
+            row1_tileImgs[row1_tileImgsPointer[i]].verschieben(0,-(i+1)*tileSize); // nach oben verschieben
         }
         for(int i = 0; i< tileCount; i++){
-            row2_tileImgs[i].sichtbarSetzen(true);
-            row2_tileImgs[i].positionSetzen(row2_TL_POS);
-            row2_tileImgs[i].verschieben(offsetX,offsetY);
-            row2_tileImgs[i].verschieben(0,-(i+1)*tileSize); // nach oben verschieben
+            row2_tileImgs[row2_tileImgsPointer[i]].sichtbarSetzen(true);
+            row2_tileImgs[row2_tileImgsPointer[i]].positionSetzen(row2_TL_POS);
+            row2_tileImgs[row2_tileImgsPointer[i]].verschieben(offsetX,offsetY);
+            row2_tileImgs[row2_tileImgsPointer[i]].verschieben(0,-(i+1)*tileSize); // nach oben verschieben
         }
 
     }
+    private int[] giveShuffeldArray(int size){
+        int[] arraySolution = new int[size];
+        List<Integer> solution = new ArrayList<>();
+        for (int i = 0; i < size; i++){ //to generate from 0-10 inclusive.
+            //For 0-9 inclusive, remove the = on the <=
+            solution.add(i);
+        }
+        Collections.shuffle(solution);
+        for (int i = 0; i < solution.size(); i++) {
+            arraySolution[i] = solution.get(i);
+        }
+        return arraySolution;
 
-    public void lowerSpeed() {
-        speed--;
     }
+
     public boolean isActive() {
         return active;
     }
+
 }

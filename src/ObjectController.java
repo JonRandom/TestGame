c2c5b84e8b1644f0a09296f180ff0fbe79a0418d
simonh2002 <67796170+simonh2002@users.noreply.github.com>
2020-11-25 20:08:18 +0100

    Autos fahren um Häuserblöcke

--- src/ObjectController.java
+++ src/ObjectController.java
@@ -1,6 +1,4 @@
-import ea.Bild;
-import ea.Knoten;
-import ea.Vektor;
+import ea.*;
 
 /**
  * Steuert z.B Autos
@@ -11,22 +9,26 @@ public class ObjectController extends Knoten{
 
     public ObjectController(){
 
-        Auto1= new Auto("./Assets/Tests/car.png");
+        Auto1= new Auto("./Assets/Tests/Testauto.png");
         this.add(Auto1);
     }
 
 
 
-    private class Auto extends Knoten {
-        private Bild pic;
+    public class Auto extends Knoten {
+        public Bild pic;
         private Auto(String path){
-            pic = new Bild(12,12,path);
+            pic = new Bild(220,1550,path);
             this.add(pic);
             pic.newtonschMachen();
-            pic.geschwindigkeitSetzen(new Vektor(1,1));
+            pic.geschwindigkeitSetzen(new Vektor(2,0));
             pic.passivMachen();
 
+            //animationsManager.streckenAnimation(pic,20000, true, true, new Punkt(220, 1550), new Punkt(5300,1550),new Punkt(5300, 2500),new Punkt(220,2500));
 
         }
     }
+
+
+
 }
